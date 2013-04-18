9c90e63e1f87b34e8b431d39cb79651f8d0e6b01
James Robinson <admin@robinjam.net>
2013-04-18 09:22:57 +0100

    Change KNN classifier tests to Test::Unit style

--- test/test_knn_classifier.rb
+++ test/test_knn_classifier.rb
@@ -1,18 +1,18 @@
 require 'minitest/autorun'
 require 'knn_classifier'
 
-describe KnnClassifier do
-  before do
+class TestKnnClassifier < MiniTest::Unit::TestCase
+  def setup
     @knn = KnnClassifier.new [0, 0] => "A", [5, 5] => "A", [10, 10] => "B", [15, 15] => "B"
   end
 
-  it "classifies points by calculating nearest neighbours" do
-    @knn.classify([6, 6], 3).must_equal "A"
-    @knn.classify([9, 9], 3).must_equal "B"
+  def test_classify
+    assert_equal "A", @knn.classify([6, 6], 3)
+    assert_equal "B", @knn.classify([9, 9], 3)
   end
 
-  it "reduces k and tries again if there is a tie" do
-    @knn.classify([6, 6], 4).must_equal "A"
-    @knn.classify([9, 9], 4).must_equal "B"
+  def test_classify_with_tie
+    assert_equal "A", @knn.classify([6, 6], 4)
+    assert_equal "B", @knn.classify([9, 9], 4)
   end
 end
