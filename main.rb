1c333f62d382becb4d2bad7e9ba6a36e0449c8f4
James Robinson <admin@robinjam.net>
2013-04-18 06:36:06 +0100

    Try different combinations of variables

--- main.rb
+++ main.rb
@@ -24,28 +24,46 @@ def read_data(filename)
   standard_deviations = variables.map(&:standard_deviation)
 
   # Normalize the data by subtracting the mean and dividing by the standard deviation, and return the result
-  lines.map do |point, classification|
+  lines.map! do |point, classification|
     [point.each_with_index.map { |e, i| (e - means[i]) / standard_deviations[i] }, classification]
   end
+
+  [lines, variables]
 end
 
 def main
   # Read the dive data
-  lines = read_data("diveData.txt")
+  lines, variables = read_data("diveData.txt")
 
   # Output a CSV file showing the accuracy of the classifier with different values of k
-  puts "k,Accuracy"
-  1.upto(lines.count) do |k|
-    accuracy = lines.map do |point, classification|
-      # Create a KNN classifier, excluding the current point from the dataset
-      knn = KnnClassifier.new(lines.reject { |p, _| p == point })
+  puts "k,Variables,Accuracy"
 
-      # Check if the classification was correct
-      knn.classify(point, k) == classification
-    end.count(true) / lines.count.to_f
+  # Try all possible combinations of variables
+  1.upto(2 ** variables.count - 1) do |n|
+    # Try values from 1-10 for k (larger values typically produce poor results)
+    1.upto(10) do |k|
+      accuracy = lines.map do |point, classification|
+        # Filter the current point from the dataset
+        filtered_lines = lines.reject { |p, _| p == point }.
 
-    puts "#{k},#{accuracy}"
-    $stdout.flush
+        # Exclude variables we're not using
+        # Ruby allows us to index integers as if they were arrays of bits,
+        # so we can check if a variable should be used by testing if the ith bit of n is 1
+        map { |line| [line.first.each_with_index.map { |e, i| e if n[i] == 1 }.compact, line.last] }
+
+        # Create a KNN classifier using the filtered set
+        knn = KnnClassifier.new(filtered_lines)
+
+        point = point.each_with_index.map { |e, i| e if n[i] == 1 }.compact
+
+        # Check if the classification was correct
+        knn.classify(point, k) == classification
+      end.count(true) / lines.count.to_f
+
+      vars = 1.upto(variables.count).each_with_index.map { |e, i| e if n[i] == 1 }.compact.join(" ")
+      puts "#{k},#{vars},#{accuracy}"
+      $stdout.flush
+    end
   end
 end
 
